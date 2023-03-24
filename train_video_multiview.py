20d19ec7674ba5e68cc26eb6282afd4f7556ad83
Lili Karashchuk <krchtchk@gmail.com>
2023-03-24 15:52:39 -0700

    clean up the train video scripts

--- train_video_multiview.py
+++ train_video_multiview.py
@@ -11,16 +11,18 @@ from config.config_reader import parse_args, create_parser
 
 from dataloader.load_dataloader import load_dataloader
 
-from model.unsupervised_model import Model
+from model.unsupervised_model_seg_vol_edge_tree import Model
 
 from loss.compute_loss import *
 
-from utils import Logger, mkdir_p, save_images, save_3d_images, save_multi_images
+from utils import Logger, mkdir_p, save_images, save_3d_images, save_multi_images,save_images_2
 from utils.model_utils import *
+from utils.triangulation import Camera
 import cv2
 import numpy as np
 import copy
 
+import gc
 
 best_loss = 10000
 
@@ -44,7 +46,11 @@ def main_worker(gpu, ngpus_per_node, args):
 
     # create model
     output_shape = (int(args.image_size/4), int(args.image_size/4))
-    model = Model(args.nkpts, output_shape=output_shape)
+    model = Model(args.nkpts, output_shape=output_shape,
+                  volume_size=args.volume_size,
+                  cuboid_side=args.cuboid_side,
+                  v2v_features=args.v2v_features)
+
 
     if args.gpu is not None:
         torch.cuda.set_device(args.gpu)
@@ -58,6 +64,16 @@ def main_worker(gpu, ngpus_per_node, args):
     optimizer = torch.optim.SGD(model.parameters(), args.lr,
                                 momentum=args.momentum,
                                 weight_decay=args.weight_decay)
+
+
+    num_edges = args.nkpts*(args.nkpts-1)//2
+    edge_weights = torch.randn(num_edges, requires_grad=True, device = torch.device('cuda'))
+    optimizer_2 = torch.optim.SGD([edge_weights], args.lr*512,
+                                momentum=args.momentum,
+                                weight_decay=args.weight_decay)
+
+    running_average = torch.zeros(num_edges, requires_grad=False, device = torch.device('cuda'))
+
 
     # optionally resume from a checkpoint
     if not os.path.isdir(args.checkpoint):
@@ -75,6 +91,11 @@ def main_worker(gpu, ngpus_per_node, args):
                 checkpoint = torch.load(args.resume, map_location=loc)
             args.start_epoch = checkpoint['epoch']
             best_loss = checkpoint['best_loss']
+
+            edge_weights = checkpoint['edge_weights']
+            optimizer_2.load_state_dict(checkpoint['optimizer_2'])
+
+            running_average = checkpoint['running_average']
 
             model.load_state_dict(checkpoint['state_dict'])
             optimizer.load_state_dict(checkpoint['optimizer'])
@@ -98,11 +119,13 @@ def main_worker(gpu, ngpus_per_node, args):
 
     train_loader = torch.utils.data.DataLoader(
         train_dataset, batch_size=args.batch_size, shuffle=(train_sampler is None),
-        num_workers=args.workers, pin_memory=True, sampler=train_sampler)
+        num_workers=args.workers, pin_memory=True,
+        sampler=train_sampler)
 
     val_loader = torch.utils.data.DataLoader(
         val_dataset, batch_size=args.batch_size, shuffle=False,
-        num_workers=args.workers, pin_memory=True)
+        num_workers=args.workers, pin_memory=True
+    )
 
     if args.evaluate:
         validate(val_loader, model, loss_module, 0, args)
@@ -113,7 +136,7 @@ def main_worker(gpu, ngpus_per_node, args):
         adjust_learning_rate(optimizer, epoch, args)
 
         # train for one epoch
-        train_loss = train(train_loader, model, loss_module, loss_3d, optimizer, epoch, args)
+        train_loss, running_average = train(train_loader, model, edge_weights, running_average, loss_module, loss_3d, optimizer, optimizer_2, epoch, args)
 
         # evaluate on validation set every val_schedule epochs
         if epoch > 0 and epoch%args.val_schedule == 0:
@@ -132,103 +155,15 @@ def main_worker(gpu, ngpus_per_node, args):
             'state_dict': model.state_dict(),
             'best_loss': best_loss,
             'optimizer' : optimizer.state_dict(),
+            'edge_weights': edge_weights,
+            'optimizer_2' : optimizer_2.state_dict(),    
+            'running_average': running_average        
         }, is_best, checkpoint=args.checkpoint)
 
-#############################
-
-def undistort_torch(points, matrix, dist):
-    mat = torch.linalg.inv(matrix)
-    pts = torch.linalg.matmul(points, mat[:2,:2]) + mat[:2, 2]
-
-    # translated from OpenCV undistortPoints code
-    x0 = x = pts[:, 0]
-    y0 = y = pts[:, 1]
-    k = dist
-
-    ITERS = 5
-    for j in range(ITERS):
-        r2 = torch.square(x) + torch.square(y)
-        icdist = 1/(1 + ((k[4]*r2 + k[1])*r2 + k[0])*r2)
-        deltaX = 2*k[2]*x*y + k[3]*(r2 + 2*x*x)
-        deltaY = k[2]*(r2 + 2*y*y) + 2*k[3]*x*y
-        x = (x0 - deltaX)*icdist
-        y = (y0 - deltaY)*icdist
-
-    return torch.stack([x, y]).T
-
-def distort_torch(points, matrix, dist):
-    x = points[:, 0]
-    y = points[:, 1]
-    k = dist
-    r2 = torch.square(x) + torch.square(y)
-    x_new = x * (1 + ((k[4]*r2 + k[1])*r2 + k[0])*r2) + 2*k[2]*x*y + k[3]*(r2 + 2*x*x)
-    y_new = y * (1 + ((k[4]*r2 + k[1])*r2 + k[0])*r2) + 2*k[3]*x*y + k[2]*(r2 + 2*y*y)
-    pts = torch.stack([x_new, y_new]).T
-    pts = torch.linalg.matmul(pts, matrix[:2,:2]) + matrix[:2, 2]
-    return pts
-
-def triangulate_simple(points, camera_mats, conf):
-    num_cams = len(camera_mats)
-    A = torch.zeros((num_cams * 2, 4)).to(points.device)
-    for i in range(num_cams):
-        x, y = points[i]
-        mat = camera_mats[i]
-        A[(i * 2):(i * 2 + 1)] = x * mat[2] - mat[0]
-        A[(i * 2 + 1):(i * 2 + 2)] = y * mat[2] - mat[1]
-
-    u, s, vh = torch.linalg.svd(A, full_matrices=True)
-    p3d = vh[-1]
-    p3d = p3d[:3] / p3d[3]
-    return p3d
-
-def triangulate_batch(points, camera_mats, scores=None):
-    """Given a CxNx2 array of points and Cx4x4 array of extrinsics,
-    this returns a Nx3 array of points,
-    where N is the number of points and C is the number of cameras.
-    Optionally also takes a CxN array of scores for points."""
-    # we build an A matrix that is (num_cams, num_points, 2d, 4)
-    num_cams = len(camera_mats)
-
-    A_base = camera_mats[:, None, 2:3].broadcast_to(num_cams, 1, 2, 4)
-    A = points[:, :, :,None] * A_base - camera_mats[:, None, :2]
-    if scores is not None:
-        A = A * scores[:, :, None, None]
-    # now shape A matrix to (num_points, num_cams*2, 4)
-    A = A.swapaxes(0, 1).reshape(-1, num_cams*2, 4)
-    u, s, vh = torch.linalg.svd(A, full_matrices=True)
-    p3d = vh[:, -1]
-    p3d = p3d[:, :3] / p3d[:,3:4]
-    return p3d
-
-def project_points(points_3d, camera_params):
-    extrinsics = camera_params['extrinsics']
-    intrinsics = camera_params['intrinsics']
-    distortions = camera_params['distortions']
-    proj_2d = []
-    for i in range(len(intrinsics)):
-        proj_und = torch.matmul(points_3d, extrinsics[i][:3,:3].T) + extrinsics[i][:3, 3]
-        pts = proj_und[:, :2] / proj_und[:,2:]
-        p = distort_torch(pts, intrinsics[i], distortions[i])
-        proj_2d.append(p)
-    proj_2d = torch.stack(proj_2d)
-    return proj_2d
-
-def triangulate_points_full(points_2d, camera_params, confidence=None):
-    extrinsics = camera_params['extrinsics']
-    intrinsics = camera_params['intrinsics']
-    distortions = camera_params['distortions']
-    pts_2d_und = torch.stack(
-        [undistort_torch(points_2d[i], intrinsics[i], distortions[i])
-         for i in range(len(intrinsics))])
-    # pts_3d = torch.stack([triangulate_simple(pts_2d_und[:, i], extrinsics, confidence[:, i])
-    #                       for i in range(pts_2d_und.shape[1])])
-    pts_3d = triangulate_batch(pts_2d_und, extrinsics, confidence)
-    return pts_3d
-
 
 #############################
 
-def train(train_loader, model, loss_module, loss_3d, optimizer, epoch, args):
+def train(train_loader, model, edge_weights, running_average, loss_module, loss_3d, optimizer,optimizer_2, epoch, args):
 
     batch_time = AverageMeter('Time', ':6.3f')
     data_time = AverageMeter('Data', ':6.3f')
@@ -248,20 +183,27 @@ def train(train_loader, model, loss_module, loss_3d, optimizer, epoch, args):
         data_time.update(time.time() - end)
 
         # Compute reconstruction loss over each camera view
-        all_cam_points = []
-        all_cam_points_ori = []
+        all_cam_inputs = []
+        all_cam_tr_inputs = []
+        all_cams = []
 
-        all_cam_conf = []
-        all_cam_conf_ori = []
+        # all_cam_conf = []
+        # all_cam_conf_ori = []
 
-        all_cam_recon = []
-        all_cam_heatmap = []
+        # all_cam_recon = []
+        # all_cam_heatmap = []
 
-        loss = 0
+        # loss = 0
+
+        rot_im1_list = []
+        rot_im2_list = []
+        rot_im3_list = []
+
 
         for cam_num in range(len(all_cam_items['image'])):
             inputs, tr_inputs = all_cam_items['image'][cam_num]
             loss_mask, in_mask = all_cam_items['mask'][cam_num]
+            # tr versions of rotated images
             rot_im1, rot_im2, rot_im3 = all_cam_items['rotation'][cam_num]
 
             if args.gpu is not None:
@@ -273,141 +215,86 @@ def train(train_loader, model, loss_module, loss_3d, optimizer, epoch, args):
                 rot_im2 = rot_im2.cuda(args.gpu, non_blocking=True)               
                 rot_im3 = rot_im3.cuda(args.gpu, non_blocking=True)
 
-            if epoch < args.curriculum:
-                output = model(inputs, tr_inputs)
-            else:
-                output = model(inputs, tr_inputs, gmtr_x1 = rot_im1, gmtr_x2 = rot_im2, gmtr_x3 = rot_im3)
+            all_cam_inputs.append(inputs)
+            all_cam_tr_inputs.append(tr_inputs)
+            rot_im1_list.append(rot_im1)
+            rot_im2_list.append(rot_im2)
+            rot_im3_list.append(rot_im3)                        
 
-            all_cam_points.append(torch.stack(output['tr_pos'], axis=2))
-            all_cam_points_ori.append(torch.stack(output['pos'], axis=2))
+        intrinsics = all_cam_items['calib_intrinsics']
+        extrinsics = all_cam_items['calib_extrinsics']
+        distortions = all_cam_items['calib_distortions']
 
-            all_cam_conf.append(output['tr_confidence'])
-            all_cam_conf_ori.append(output['confidence'])
-            all_cam_recon.append(output['recon'])
-            all_cam_heatmap.append(output['tr_heatmap'])            
+        # print(distortions)
+        # error
 
-            loss += loss_module.update_loss(inputs, tr_inputs, loss_mask, output, epoch)
-            
+        # proj_matrices =  shape (batch_size, n_views, 3, 4)
+        # Make Camera Classes
+        # batch x cam_num x 4 x 4
+        #print(extrinsics.size())
+        for view in range(extrinsics.size()[1]):
+            curr_batch = []
+            for batch in range(extrinsics.size()[0]):
+                curr_batch.append(Camera(extrinsics[batch, view, :3, :3], extrinsics[batch, view, :3, 3], 
+                    intrinsics[batch, view], distortions[batch, view]))
 
-        ############################### Map to 3D (Loop through batch)
-        # First dimension is the batch
+            all_cams.append(curr_batch)
+
+        output = model(all_cam_inputs, all_cam_tr_inputs, edge_weights, all_cams, all_cam_items)
+
+        all_cam_points = [torch.stack(item, axis=2) for item in output['tr_pos']]
+        all_cam_points_ori = [torch.stack(item, axis=2) for item in output['pos']]
+
+
         if epoch >= args.curriculum:
+            ori_loss, length_loss, ssim_list, running_average = loss_module.update_loss_4(all_cam_inputs, all_cam_tr_inputs, running_average, output['samples'], loss_mask, output, epoch, args.nkpts)    
 
-            full_points = (torch.stack(all_cam_points, dim = 1)+1)*500
-            full_points_ori = (torch.stack(all_cam_points_ori, dim = 1)+1)*500
-
-
-            full_conf = torch.stack(all_cam_conf, dim = 1)
-            full_conf_ori = torch.stack(all_cam_conf_ori, dim = 1)
-
-            intrinsics = all_cam_items['calib_intrinsics']
-            extrinsics = all_cam_items['calib_extrinsics']
-            distortions = all_cam_items['calib_distortions']
-
-
-            if args.gpu is not None:
-                intrinsics = intrinsics.cuda(args.gpu, non_blocking=True)
-                extrinsics = extrinsics.cuda(args.gpu, non_blocking=True)
-                distortions = distortions.cuda(args.gpu, non_blocking=True)
-
-
-            projected_points = []
-            projected_points_ori = []
-            for b in range(full_points.size()[0]):
-
-                camera_params = {
-                    'extrinsics': extrinsics[b],
-                    'intrinsics': intrinsics[b],
-                    'distortions': distortions[b]
-                }
-
-                pts_3d = triangulate_points_full(full_points[b], camera_params, full_conf[b])
-
-                # reproject
-                proj_2d = project_points(pts_3d, camera_params)
-
-                projected_points.append(proj_2d/500 - 1)
-
-
-                pts_3d_ori = triangulate_points_full(full_points_ori[b], camera_params, full_conf_ori[b])
-                # reproject
-                proj_2d_ori = project_points(pts_3d_ori, camera_params)
-
-                projected_points_ori.append(proj_2d_ori/500 - 1)
-
-
-            projected_points = torch.stack(projected_points, dim = 0).clip(-1, 1)
-            projected_points_ori = torch.stack(projected_points_ori, dim = 0).clip(-1, 1)
-
-            ## 3D Separation Loss
-            # TODO: Make the loss module a dictionary
-            sep_loss = loss_module.criterion[2]
-            loss += sep_loss.loss_3d(projected_points)
-            loss += sep_loss.loss_3d(projected_points_ori)            
-
-            tester = (full_points/500-1).clone().detach()
-            loss2 = (torch.sum((projected_points - tester) ** 2, dim = -1)).mean()
-            loss += loss2
-            print("Reprojection: " + str(loss2))
-
-            ##########################################
-            # Multiview reconstruction loss
-            # TODO: Also add rotation loss here?
-            for cam_num in range(len(all_cam_items['image'])):
-                inputs, tr_inputs = all_cam_items['image'][cam_num]
-                loss_mask, in_mask = all_cam_items['mask'][cam_num]
-
-                if args.gpu is not None:
-                    inputs = inputs.cuda(args.gpu, non_blocking=True)
-                    tr_inputs = tr_inputs.cuda(args.gpu, non_blocking=True)
-                    loss_mask = loss_mask.cuda(args.gpu, non_blocking=True)
-                    in_mask = in_mask.cuda(args.gpu, non_blocking=True)
-                
-                output_mv = model.cross_view_recon(inputs, projected_points_ori[:, cam_num, :, 0],
-                    projected_points_ori[:, cam_num, :, 1], projected_points[:, cam_num, :, 0],
-                    projected_points[:, cam_num, :, 1])
-
-                loss3 = loss_module.update_loss(inputs, tr_inputs, loss_mask, output_mv, epoch)
-                loss += loss3
-
-            print("Reprojection reconstruction: " + str(loss3))
-
-
-        ##########################################
+            loss = ori_loss + length_loss/1000000
+        else:
+            ori_loss, ssim_list = loss_module.update_loss_2(all_cam_inputs, all_cam_tr_inputs, loss_mask, output, epoch)    
+            loss = ori_loss
 
         # measure accuracy and record loss
         losses.update(loss.item(), inputs.size(0))
 
         # compute gradient and do SGD step
         optimizer.zero_grad()
+        optimizer_2.zero_grad()
         loss.backward()
         torch.nn.utils.clip_grad_norm_(model.parameters(), 10.0)
         optimizer.step()
 
+        optimizer_2.step()
+
         # measure elapsed time
         batch_time.update(time.time() - end)
         end = time.time()
+
+        gc.collect()
 
         if i % args.print_freq == 0:
             progress.display(i)
             
             if args.visualize:
 
-                save_multi_images(all_cam_items['image'], all_cam_points, all_cam_recon, 
-                    all_cam_heatmap, all_cam_conf, epoch, args, epoch)                
+                # print(all_cam_points)
+                save_multi_images(all_cam_items['image'], all_cam_points_ori,
+                    all_cam_points, output['recon'], ssim_list,
+                    output['tr_kpt_out'], output['tr_kpt_cond'], 
+                    [output['tr_confidence'] for _ in range(len(all_cam_items['image']))], epoch, args, epoch)
                 # print(output['tr_pos'][0].size(), projected_points[:, :, 0].size())
-                save_3d_images(pts_3d.detach().cpu(), epoch, args, epoch)
+                # save_3d_images(pts_3d.detach().cpu(), epoch, args, epoch)
 
 
-                save_images(tr_inputs, output, epoch, args, epoch)
+                # save_images_2(tr_inputs, output, epoch, args, epoch)
 
-                if epoch >= args.curriculum:
+                # if epoch >= args.curriculum:
 
-                    output['recon'] = output_mv['recon']
-                    output['tr_pos'] = (projected_points[:, -1, :, 0], projected_points[:, -1, :, 1])
-                    save_images(tr_inputs, output, epoch, args, str(epoch) + 'projected')
+                #     output['recon'] = output_mv['recon']
+                #     output['tr_pos'] = (projected_points[:, -1, :, 0], projected_points[:, -1, :, 1])
+                #     save_images(tr_inputs, output, epoch, args, str(epoch) + 'projected')
 
-    return losses.avg
+    return losses.avg, running_average
 
 def validate(val_loader, model, loss_module, epoch, args):
 
